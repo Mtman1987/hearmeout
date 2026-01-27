746756c17412c35d4cf526dfa7ae59cd485cc767
Mt Man <mtman1987@gmail.com>
2026-01-27 13:07:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4331919473-dea24",
+  "appId": "1:1085802210092:web:0c9459bddf7b07cf3b0289",
+  "apiKey": "AIzaSyAhjfR7jH1JJnDEi5fF9CoracZeHatGb1A",
+  "authDomain": "studio-4331919473-dea24.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1085802210092"
+};
