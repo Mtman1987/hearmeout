d455f6c2e81bc44d0876245bbc3d23b7d3808cc4
Mt Man <mtman1987@gmail.com>
2026-01-27 17:20:42 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ src/next.config.ts
@@ -0,0 +1,41 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_LIVEKIT_URL: process.env.LIVEKIT_URL,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  allowedDevOrigins: [
+      "https://6000-firebase-studio-1769512226244.cluster-j6d3cbsvdbe5uxnhqrfzzeyj7i.cloudworkstations.dev"
+  ]
+};
+
+export default nextConfig;
